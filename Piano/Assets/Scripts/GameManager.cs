ab52c3789ccdee6ad97cf93a422c0e708adbaf77
Brady Claypool <poolofclay33@gmail.com>
2019-02-20 18:10:18 -0600

    ...

---
+++ Piano/Assets/Scripts/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+
+    public Notes _but;
+
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+
+    public TMP_Text _scoreTxt;
+    public TMP_Text _multiTxt;
+
+    public int currentMulti;
+    public int multiTracker;
+    public int[] multiThreshold;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+        _scoreTxt.text = "Score: 0";
+        currentMulti = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                _but.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+	}
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+
+        if (currentMulti - 1 < multiThreshold.Length)
+        {
+            multiTracker++;
+
+            if (multiThreshold[currentMulti - 1] <= multiTracker)
+            {
+                multiTracker = 0;
+                currentMulti++;
+            }
+        }
+
+        _multiTxt.text = "Multiplier: x" + currentMulti;
+
+        currentScore += scorePerNote * currentMulti;
+        _scoreTxt.text = "Score: " + currentScore;
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log(":(");
+
+        currentMulti = 1;
+        multiTracker = 0;
+        _multiTxt.text = "Multiplier: x" + currentMulti;
+    }
+}
---
+++ Piano/Assets/Scripts/Notes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+
+    public float beatTemp;
+    public bool hasStarted;
+
+	// Use this for initialization
+	void Start () {
+        beatTemp = beatTemp / 60f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        } else{
+            transform.position -= new Vector3(0f, beatTemp * Time.deltaTime, 0f);
+        }
+	}
+}
